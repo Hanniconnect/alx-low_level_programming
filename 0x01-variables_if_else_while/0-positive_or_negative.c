0b7ea234830fe8b8ecc2b937ef5a93e471facc48
Hanniconnect <hanniplexdacomplex@gmail.com>
2022-09-10 18:09:21 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,61 +1,45 @@
-#include <stdlib.h>                                                                                                                                                                                         
+#include <stdlib.h>
 
-#include <stdio.h>                                                                                                                                                                                          
+#include <time.h>
 
-                                                                                                                                                                                                            
+#include <stdio.h>
 
-/**                                                                                                                                                                                                         
 
-* main - print if the number is positive, zero, or negative                                                                                                                                                 
 
-*                                                                                                                                                                                                           
-
-* Description: using the main function                                                                                                                                                                      
-
-* this program prints "Programming positive, zero, or negative                                                                                                                                              
-
-* Return: 0                                                                                                                                                                                                 
-
-*/   
+/**
+ *
+ *  * main - assigns a random number to int n everytime
+ *
+ *   * it executes, and prints it
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
 
-int n;
+		int n;
 
 
 
-srand(time(0));
+			srand(time(0));
 
-n = rand() - RAND_MAX / 2;
+				n = rand() - RAND_MAX / 2;
 
-  /* your code goes there */
+					if (n > 0)
 
-if (n > 0)
+								printf("%d is positive\n", n);
 
-{
+						else if (n == 0)
 
-              printf("%1 is positive\n", n);
+									printf("%d is zero\n", n);
 
-}
+							else if (n < 0)
 
-else if (n == 0)
+										printf("%d is negative\n", n);
 
-{
-
-              printf("%1 is zero\n", n);
-
-}
-
-else if (n < 0)
-
-{
-
-              printf("%1 is negative\n", n);
-
-}
-
-return (0);
+								return (0);
 
 }
