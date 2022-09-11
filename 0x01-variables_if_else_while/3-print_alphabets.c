277dd40ab0f5ba28e3518ed0b629b5ab350c007d
Hanniconnect <hanniplexdacomplex@gmail.com>
2022-09-11 13:43:18 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /** 
  * main - prints the alphabet in lowercase, and then in uppercase,
  * followed by a new line
@@ -10,9 +11,10 @@ int main(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	putchar(ch);
+
+        	putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++);
-	putchar(ch);
+	        putchar(ch);
 	putchar('\n');
 	return (0);
 }
