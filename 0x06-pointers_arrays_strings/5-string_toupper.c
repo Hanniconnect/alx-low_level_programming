aa804cd8dd76da57e6d548abe00538fb2ad25b6f
Hanniconnect <hanniplexdacomplex@gmail.com>
2022-09-22 04:06:22 -0700

    Task string to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to modify
+ *
+ * Return: the resulting string
  */
-int print_sign(int n)
+char *string_toupper(char *s)
 {
-	if (n > 0)
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 	}
-	        return (-1);
+
+	return (s);
 }
