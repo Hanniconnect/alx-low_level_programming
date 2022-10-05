92561fe0b25be2f1e1e243f516fb3a55b1336d85
Hanniconnect <hanniplexdacomplex@gmail.com>
2022-10-04 19:36:52 -0700

    Task create array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,24 +5,25 @@
  * *create_array - creates an array of chars,
  * and initializes it with a specific char
  * @size: size of the array to create
- * @c: char to initialize the array c
- *
- * Return: pointer to the array (Success), NULL (Error)
+ * char to insert
+ * Return: NULL if size is zero or if it fails
+ * pointer to array if everything is normal
  */
+
 char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int index;
 
-	 if (size == 0)
-		 return (NULL);
-	 array = malloc(sizeof(char) * size);
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
 
-	 if (array == NULL)
-		 return (NULL);
+	if (array == NULL)
+		return (NULL);
 
-	 for (index = 0; index < size; index++)
-		 array[index] = c;
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
-	 return (array);
+	return (array);
 }
