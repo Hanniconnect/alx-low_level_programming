68d477f9bcc2b5542c256165df5d7b6b84621a93
Hanniconnect <hanniplexdacomplex@gmail.com>
2022-12-15 02:21:59 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a
- * dlistint_t list
+ * dlistint_len - returns the number of elements in
+ * a double linked list
  *
  * @h: head of the list
  * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
-
+	
 	count = 0;
 
 	if (h == NULL)
@@ -18,9 +18,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h->prev != NULL)
 		h = h->prev;
+
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
